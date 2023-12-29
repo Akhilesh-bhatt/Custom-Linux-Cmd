95ce414668451b6aee6062065bbe60aac1c72aed
Akhilesh Bhatt <88084456+Akhilesh-bhatt@users.noreply.github.com>
2023-12-29 21:53:35 +0530

    updated script

--- internsctl.sh
+++ internsctl.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Declare variables for command name and version
 command_name="internsctl"
 command_version="v0.1.0"
 
+# Function to display the manual page
 function man_page() {
+    # Use a pager to display the manual content
     echo "MANUAL PAGE FOR $command_name"
     echo "-----------------------------"
     # Replace this with your actual manual content
@@ -11,51 +14,128 @@ function man_page() {
     echo "Detailed usage guidelines and examples should be provided here."
 }
 
+# Function to display the help message
 function help_message() {
     echo "USAGE: $command_name [OPTION]"
     echo "Options:"
     echo "  --help     Display this help message."
     echo "  --version  Display the version of the command."
-    echo "Subcommands:"
-    echo "  cpu getinfo    Display CPU information."
-    echo "  memory getinfo Display memory information."
-    echo "  user create    Create a new user."
-    echo "  user list      List regular users."
-    echo "  user list --sudo-only List users with sudo permissions."
-    echo "  file getinfo  [options] <filename> Get file information."
-    echo "    Options: --size, -s  Print size"
-    echo "             --permissions, -p Print file permissions"
-    echo "             --owner, -o Print file owner"
-    echo "             --last-modified, -m Print last modified time"
 }
 
-function cpu_getinfo() {
+# Handle options
+case "$1" in
+    --help)
+        help_message
+        exit 0
+        ;;
+    --version)
+        echo "$command_name $command_version"
+        exit 0
+        ;;
+    *)
+        # Handle any other actions or subcommands here
+        echo "Unknown option: $1"
+        help_message
+        exit 1
+        ;;
+esac
+
+# Main command logic (if any)
+# Add your command's main functionality here
+cpu_getinfo() {
     lscpu
 }
 
-function memory_getinfo() {
+memory_getinfo() {
     free
 }
 
-function user_create() {
+# Part 2: User management
+user_create() {
+    # Prompt for username and password securely
     read -sp "Enter username: " username
     read -sp "Enter password: " password
+
+    # Create user with appropriate options (adjust as needed)
     useradd -m -s /bin/bash "$username"
+
+    # Set password securely
     echo "$password" | passwd --stdin "$username"
 }
 
-function user_list() {
+user_list() {
     getent passwd
 }
 
-function user_list_sudo_only() {
+user_list_sudo_only() {
     getent passwd | grep -E '^[^:]+:[^:]+:0:'
 }
 
-function file_getinfo() {
+# Handle actions and subcommands
+case "$1" in
+    # ... (Previous options from Section A)
+
+    cpu)
+        shift
+        case "$1" in
+            getinfo)
+                cpu_getinfo
+                ;;
+            *)
+                echo "Unknown CPU subcommand: $1"
+                help_message
+                ;;
+        esac
+        ;;
+
+    memory)
+        shift
+        case "$1" in
+            getinfo)
+                memory_getinfo
+                ;;
+            *)
+                echo "Unknown memory subcommand: $1"
+                help_message
+                ;;
+        esac
+        ;;
+
+    user)
+        shift
+        case "$1" in
+            create)
+                user_create
+                ;;
+            list)
+                user_list
+                ;;
+            list--sudo-only)
+                user_list_sudo_only
+                ;;
+            *)
+                echo "Unknown user subcommand: $1"
+                help_message
+                ;;
+        esac
+        ;;
+
+    *)
+        echo "Unknown option: $1"
+        help_message
+        ;;
+esac
+
+#!/bin/bash
+
+# ... (Previous code from Sections A and B)
+
+# Section C: File information
+file_getinfo() {
     local filename="$1"
     local options=()
 
+    # Parse options
     while [[ $# -gt 1 ]]; do
         case "$2" in
             --size | -s)
@@ -71,23 +151,26 @@ function file_getinfo() {
                 options+=(last_modified)
                 ;;
             *)
-                echo "Unknown option: <span class="math-inline">2"
-help\_message
-return 1
-;;
-esac
-shift 2
-done
-owner\=</span>(stat -c %U "<span class="math-inline">filename"\)
-permissions\=</span>(stat -c %A "<span class="math-inline">filename"\)
-size\=</span>(stat -c %s "<span class="math-inline">filename"\)
-last\_modified\=</span>(date +"%Y-%m-%d %H:%M:%S %:z" --reference="$filename")
+                echo "Unknown option: $2"
+                help_message
+                return 1
+                ;;
+        esac
+        shift 2
+    done
 
+    # Get file information
+    owner=$(stat -c %U "$filename")
+    permissions=$(stat -c %A "$filename")
+    size=$(stat -c %s "$filename")
+    last_modified=$(date +"%Y-%m-%d %H:%M:%S %:z" --reference="$filename")
+
+    # Print output based on options
     if [[ ${#options[@]} -eq 0 ]]; then
         printf "File: %s\nAccess: %s\nSize(B): %s\nOwner: %s\nModify: %s\n" \
-            "$filename" "$permissions" "$size" "$owner" "<span class="math-inline">last\_modified"
-else
-for option in "</span>{options[@]}"; do
+            "$filename" "$permissions" "$size" "$owner" "$last_modified"
+    else
+        for option in "${options[@]}"; do
             case "$option" in
                 size)
                     echo "$size"
@@ -106,23 +189,26 @@ for option in "</span>{options[@]}"; do
     fi
 }
 
+# Handle actions and subcommands
 case "$1" in
-    --help)
-        help_message
-        ;;
-    --version)
-        echo "$command_name $command_version"
-        ;;
-    cpu)
+    # ... (Previous options from Sections A and B)
+
+    file)
         shift
         case "$1" in
             getinfo)
-                cpu_getinfo
+                shift
+                file_getinfo "$@"
                 ;;
             *)
-                echo "Unknown CPU subcommand: $1"
+                echo "Unknown file subcommand: $1"
                 help_message
                 ;;
         esac
         ;;
-    memory)
+
+    *)
+        echo "Unknown option: $1"
+        help_message
+        ;;
+esac
